26fbd8e9bab9ab86f1e94bbe9f111d397aab7842
paarig <gopalpaari@gmail.com>
2017-10-22 06:58:44 -0700

    Continued working on JSON

--- src/main/java/com/dubhacks/table/Student.java
+++ src/main/java/com/dubhacks/table/Student.java
@@ -1,9 +1,16 @@
 package com.dubhacks.table;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Student {
 
+    @JsonProperty(value = "Name", required = true)
     private String name;
+    @JsonProperty(value = "NetId", required = true)
     private String netId;
+    @JsonProperty(value = "BitMask", required = true)
     private String bitMask;
 
     public Student(String name, String netId, String bitMask)
@@ -12,32 +19,36 @@ public class Student {
         this.netId = netId;
         this.bitMask = bitMask;
     }
-
+    @JsonAnySetter
     public void setName(String name)
     {
         this.name = name;
     }
-
+    @JsonAnySetter
     public void setNetId(String netId)
     {
         this.netId = netId;
     }
 
+    @JsonAnySetter
     public void setBitMask(String bitMask)
     {
         this.bitMask = bitMask;
     }
 
+    @JsonAnyGetter
     public String getName()
     {
         return this.name;
     }
 
+    @JsonAnyGetter
     public String getNetId()
     {
         return this.netId;
     }
 
+    @JsonAnyGetter
     public String getBitMask()
     {
         return this.bitMask;
